ecc6beb692db261881d80f4560fbc0267e1f908a
Imuli <i@imu.li>
2021-06-30 06:33:04 -0400

    time reading instances for StateT

--- src/Data/Polytime/Class.hs
+++ src/Data/Polytime/Class.hs
@@ -11,11 +11,25 @@ module Data.Polytime.Class where
 
 import           Control.Monad.Trans.Class (MonadTrans, lift)
 import           Control.Monad.Trans.Reader (ReaderT, ask)
+import qualified Control.Monad.Trans.State.Lazy as Lazy (StateT, get)
+import qualified Control.Monad.Trans.State.Strict as Strict (StateT, get)
 
 -- | Fetch the current time of a particular type.
 class TimeSource time f where
   -- | What time is it now?
   now :: f time
+
+-- | Extract the current time from a lazy @time@ 'Lazy.StateT'
+--
+-- This overlaps the generic lifting through Monad transformers.
+instance {-# OVERLAPPING #-} Monad f => TimeSource time (Lazy.StateT time f) where
+  now = Lazy.get
+
+-- | Extract the current time from a strict @time@ 'Strict.StateT'
+--
+-- This overlaps the generic lifting through Monad transformers.
+instance {-# OVERLAPPING #-} Monad f => TimeSource time (Strict.StateT time f) where
+  now = Strict.get
 
 -- | Extract the current time from a @time@ 'ReaderT'
 --
